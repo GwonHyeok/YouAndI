f67eb18c11c08c37e1b5cad0f1edebece9194976
ranbly <mmimics@gmail.com>
2015-11-15 18:19:23 +0900

    [디자인] 로그인, 회원가입, 상대방연결 수정

---
+++ app/src/main/java/com/hyeokran/youi/Activity/LoginActivity.java
@@ -0,0 +1,19 @@
+package com.hyeokran.youi.Activity;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+import com.hyeokran.youi.R;
+
+/**
+ * 로그인 액티비티
+ * Created by uran on 15. 11. 15..
+ */
+public class LoginActivity extends AppCompatActivity {
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login);
+    }
+}

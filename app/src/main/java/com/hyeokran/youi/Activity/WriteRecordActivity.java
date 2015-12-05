e3bea8a75ab2d2e45e4238cb9fa9b5e17fbf62e4
GwonHyeok <kh4975@naver.com>
2015-12-06 02:10:17 +0900

    [기능구현] 글쓰기 메인 데이트 픽커 구현

--- app/src/main/java/com/hyeokran/youi/Activity/WriteRecordActivity.java
+++ app/src/main/java/com/hyeokran/youi/Activity/WriteRecordActivity.java
@@ -1,8 +1,6 @@
 package com.hyeokran.youi.Activity;
 
 import android.app.DatePickerDialog;
-import android.content.Intent;
-import android.media.Image;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
@@ -15,8 +13,11 @@ import android.widget.Toast;
 import com.hyeokran.youi.R;
 import com.sangcomz.fishbun.FishBun;
 
+import java.text.SimpleDateFormat;
 import java.util.ArrayList;
-import java.util.Timer;
+import java.util.Calendar;
+import java.util.Date;
+import java.util.Locale;
 
 /**
  * Created by uran on 15. 12. 5..
@@ -44,7 +45,6 @@ public class WriteRecordActivity extends AppCompatActivity {
         write_select_photo = (ImageView) findViewById(R.id.write_select_photo);
         write_back = (ImageView) findViewById(R.id.write_back);
 
-
         write_change_range.setOnClickListener(new View.OnClickListener() {
 
             @Override
@@ -54,16 +54,32 @@ public class WriteRecordActivity extends AppCompatActivity {
             }
         });
 
-//        write_change_date.setOnClickListener(new View.OnClickListener() {
-//            @Override
-//            public void onClick(View v) {
-//                DatePickerDialog dialog = new DatePickerDialog(getApplicationContext(), listener, 2013, 10, 22);
-//                dialog.show();
-//
-//            }
-//
-//
-//        });
+        write_change_date.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Calendar calendar = Calendar.getInstance();
+                int year = calendar.get(Calendar.YEAR);
+                int month = calendar.get(Calendar.MONTH);
+                int day = calendar.get(Calendar.DAY_OF_MONTH);
+
+                new DatePickerDialog(v.getContext(), new DatePickerDialog.OnDateSetListener() {
+                    @Override
+                    public void onDateSet(DatePicker view, int year, int monthOfYear, int dayOfMonth) {
+                        try {
+                            String dateString = String.format("%d-%d-%d", year, monthOfYear + 1, dayOfMonth);
+                            Date date = new SimpleDateFormat("yyyy-M-d", Locale.getDefault()).parse(dateString);
+                            String dayOfWeek = new SimpleDateFormat("EEEEE", Locale.ENGLISH).format(date);
+
+                            write_change_date.setText(
+                                    String.format("%d.%d.%d (%s)", year, monthOfYear, dayOfMonth + 1, dayOfWeek)
+                            );
+                        } catch (Exception e) {
+                            e.printStackTrace();
+                        }
+                    }
+                }, year, month, day).show();
+            }
+        });
 
         write_select_photo.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -71,7 +87,7 @@ public class WriteRecordActivity extends AppCompatActivity {
                 FishBun.with(v.getContext())
                         .setAlbumThumnaliSize(150)
                         .setActionBarColor(
-                                getResources().getColor(R.color.colorPrimary),getResources().getColor(R.color.colorPrimaryDark))
+                                getResources().getColor(R.color.colorPrimary), getResources().getColor(R.color.colorPrimaryDark))
                         .setPickerCount(20)
                         .setArrayPaths(mPhotoUriList)
                         .startAlbum();
@@ -87,20 +103,13 @@ public class WriteRecordActivity extends AppCompatActivity {
     }
 
     private void toggleWriteMode() {
-        if (isPrivateMode) {
-            write_private_mode_change.setText("전체공개");
-            write_change_range_icon.setImageResource(R.drawable.ic_global);
-            Toast toast = Toast.makeText(WriteRecordActivity.this, "공개범위가 '전체공개'로 변경되었습니다."
-                    , Toast.LENGTH_SHORT);
-            toast.show();
-            isPrivateMode = false;
-        } else {
-            write_private_mode_change.setText("우리끼리만");
-            write_change_range_icon.setImageResource(R.drawable.ic_rocked_blue);
-            Toast toast = Toast.makeText(WriteRecordActivity.this, "공개범위가 '비공개'로 변경되었습니다."
-                    , Toast.LENGTH_SHORT);
-            isPrivateMode = true;
-            toast.show();
-        }
+        write_private_mode_change.setText(isPrivateMode ? "전체공개" : "우리끼리만");
+        write_change_range_icon.setImageResource(isPrivateMode ? R.drawable.ic_global : R.drawable.ic_rocked_blue);
+        Toast.makeText(WriteRecordActivity.this,
+                isPrivateMode ? "공개범위가 '전체공개'로 변경되었습니다." : "공개범위가 '비공개'로 변경되었습니다.",
+                Toast.LENGTH_SHORT).show();
+
+        /* Toggle */
+        isPrivateMode = !isPrivateMode;
     }
 }
--- app/src/main/java/com/hyeokran/youi/Fragment/MainFragment.java
+++ app/src/main/java/com/hyeokran/youi/Fragment/MainFragment.java
@@ -1,12 +1,18 @@
 package com.hyeokran.youi.Fragment;
 
+import android.app.DatePickerDialog;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.DatePicker;
+import android.widget.TextView;
 
 import com.hyeokran.youi.R;
+
+import java.util.Calendar;
+import java.util.Date;
 
 /**
  * 홈 화면 Fragment
@@ -14,6 +20,10 @@ import com.hyeokran.youi.R;
  */
 public class MainFragment extends Fragment {
     private static MainFragment instance = null;
+
+    /* 상단 디데이 관련 정보 */
+    private TextView mTopDDayTextView;
+    private Date dDayDate = new Date();
 
     public synchronized static MainFragment getInstance() {
         if (instance == null) {
@@ -30,5 +40,46 @@ public class MainFragment extends Fragment {
 
     @Override
     public void onViewCreated(View view, Bundle savedInstanceState) {
+        mTopDDayTextView = (TextView) view.findViewById(R.id.main_top_dday_textview);
+        mTopDDayTextView.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                /* 디데이 클릭 하면 디데이 수정 가능 */
+                Calendar calendar = Calendar.getInstance();
+                int year = calendar.get(Calendar.YEAR);
+                final int month = calendar.get(Calendar.MONTH);
+                int day = calendar.get(Calendar.DAY_OF_MONTH);
+
+                /* Show DatePicker Dialog */
+                new DatePickerDialog(getContext(), new DatePickerDialog.OnDateSetListener() {
+                    @Override
+                    public void onDateSet(DatePicker view, int year, int monthOfYear, int dayOfMonth) {
+                        Calendar newCalendar = Calendar.getInstance();
+
+                        newCalendar.set(year, monthOfYear, dayOfMonth);
+                        dDayDate = newCalendar.getTime();
+
+                        invalidateDDay();
+                    }
+                }, year, month, day).show();
+            }
+        });
+    }
+
+    /**
+     * 상단 디데이 정보 설정
+     */
+    private void invalidateDDay() {
+        if (dDayDate == null) {
+            dDayDate = Calendar.getInstance().getTime();
+        }
+
+        int currentTime = (int) ((Calendar.getInstance().getTimeInMillis()) / (24 * 60 * 60 * 1000));
+        int startTime = (int) ((dDayDate.getTime()) / (24 * 60 * 60 * 1000));
+
+        int dDay = startTime - currentTime;
+        dDay += 1; // 1일부터 시작일 경우 1추가
+
+        mTopDDayTextView.setText(String.valueOf(dDay));
     }
 }

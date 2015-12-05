09b68b99f8eabc75720003006fec714538787961
GwonHyeok <kh4975@naver.com>
2015-12-06 02:55:57 +0900

    [기능구현] 글쓰기 사진 업로드

--- app/src/main/java/com/hyeokran/youi/Activity/WriteRecordActivity.java
+++ app/src/main/java/com/hyeokran/youi/Activity/WriteRecordActivity.java
@@ -1,17 +1,21 @@
 package com.hyeokran.youi.Activity;
 
 import android.app.DatePickerDialog;
+import android.content.Intent;
 import android.os.Bundle;
-import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
 import android.view.View;
 import android.widget.DatePicker;
 import android.widget.ImageView;
 import android.widget.LinearLayout;
+import android.widget.RelativeLayout;
 import android.widget.TextView;
 import android.widget.Toast;
 
+import com.bumptech.glide.Glide;
 import com.hyeokran.youi.R;
 import com.sangcomz.fishbun.FishBun;
+import com.sangcomz.fishbun.define.Define;
 
 import java.text.SimpleDateFormat;
 import java.util.ArrayList;
@@ -22,16 +26,19 @@ import java.util.Locale;
 /**
  * Created by uran on 15. 12. 5..
  */
-public class WriteRecordActivity extends AppCompatActivity {
-    LinearLayout write_change_range;
-    TextView write_private_mode_change;
-    TextView write_change_date;
-    ImageView write_select_photo;
-    ImageView write_change_range_icon;
-    ImageView write_back;
+public class WriteRecordActivity extends BaseToolbarActivity {
+    private LinearLayout write_change_range;
+    private TextView write_private_mode_change;
+    private TextView write_change_date;
+    private ImageView write_select_photo;
+    private ImageView write_change_range_icon;
+    private ImageView write_back;
     private boolean isPrivateMode = true;
 
-    private ArrayList<String> mPhotoUriList = new ArrayList<>();
+    /* 사진 미리보기 */
+    private View mPhotoRootScrollView;
+    private LinearLayout mPhotoRootView;
+    private ArrayList<String> mPhotoUriList;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -100,6 +107,57 @@ public class WriteRecordActivity extends AppCompatActivity {
                 finish();
             }
         });
+
+        mPhotoRootScrollView = findViewById(R.id.write_photo_scrollview);
+        mPhotoRootView = (LinearLayout) findViewById(R.id.write_photo_root);
+        invalidatePhotoView();
+    }
+
+    protected void onActivityResult(int requestCode, int resultCode, Intent imageData) {
+        super.onActivityResult(requestCode, resultCode, imageData);
+        switch (requestCode) {
+            case Define.ALBUM_REQUEST_CODE:
+                if (resultCode == RESULT_OK) {
+                    this.mPhotoUriList = imageData.getStringArrayListExtra(Define.INTENT_PATH);
+                    invalidatePhotoView();
+                    break;
+                }
+        }
+    }
+
+    /**
+     * 사진 미리보기 뷰 정리
+     */
+    private void invalidatePhotoView() {
+        if (mPhotoUriList == null || mPhotoUriList.size() == 0) {
+            Log.d(getClass().getSimpleName(), " SIZE IS 0");
+            mPhotoRootScrollView.setVisibility(View.GONE);
+            mPhotoRootView.removeAllViews();
+            return;
+        }
+
+        mPhotoRootView.removeAllViews();
+        for (int i = 0; i < mPhotoUriList.size(); i++) {
+            int width = getResources().getDimensionPixelOffset(R.dimen.write_photo_thumbnail_width);
+            int height = getResources().getDimensionPixelOffset(R.dimen.write_photo_thumbnail_height);
+            int padding = getResources().getDimensionPixelOffset(R.dimen.write_photo_thumbnail_padding);
+
+            ImageView imageView = new ImageView(this);
+            imageView.setLayoutParams(new RelativeLayout.LayoutParams(width, height));
+            imageView.setPadding(i == 0 ? padding * 2 : padding, 0, padding, 0);
+
+            Glide.with(this)
+                    .load(mPhotoUriList.get(i))
+                    .asBitmap()
+                    .centerCrop()
+                    .override(Define.ALBUM_THUMNAIL_SIZE, Define.ALBUM_THUMNAIL_SIZE)
+                    .into(imageView);
+
+            mPhotoRootView.addView(imageView);
+        }
+
+        mPhotoRootScrollView.setVisibility(View.VISIBLE);
+        mPhotoRootView.setVisibility(View.VISIBLE);
     }
 
     private void toggleWriteMode() {
